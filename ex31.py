f8c0dd055c72e4b41f56d95f7168e83254161672
ZueZueMyatMaw2261997 <zuezuemyatmawlay1997@gmail.com>
2018-06-12 16:42:15 +0630

    ex31

--- ex31.py
+++ ex31.py
@@ -1,6 +1,5 @@
- 
 print("""You enter a dark room with two doors. 
-        Do you go through door #1 or door #2?""") 
+Do you go through door #1 or door #2?""") 
 
 door = input("> ") 
 
@@ -20,21 +19,20 @@ else:
     print(f"Well, doing {bear} is probably better.") 
     print("Bear runs away.") 
         
-elif door == "2": 
-    print("You stare into the endless abyss at Cthulhu's retina." 
+ 
+    print("You stare into the endless abyss at Cthulhu's retina.") 
     print("1. Blueberries.") 
     print("2. Yellow jacket clothespins.")
-
     print("3. Understanding revolvers yelling melodies.") 
                 
     insanity = input("> ") 
                 
-if insanity == "1" or insanity == "2": 
-        print("Your body survives powered by a mind of jello." 
+    if insanity == "1" or insanity == "2": 
+        print("Your body survives powered by a mind of jello.") 
         print("Good job!") 
-else: 
-        print("The insanity rots your eyes into a pool of muck." 
+    else: 
+        print("The insanity rots your eyes into a pool of muck.") 
         print("Good job!") 
                         
-else: 
-    print("You stumble around and fall on a knife and die. Good job!" 
+ 
+    print("You stumble around and fall on a knife and die. Good job!") 
